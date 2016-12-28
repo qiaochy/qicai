96b68f22cfef3ff8086e275a925501d9b9d608f1
qiaochy <892689739@qq.com>
2016-12-28 15:00:20 +0800

    12345

--- QiCai/index.php
+++ QiCai/index.php
@@ -8,17 +8,31 @@
 //定义网站根目录
 define('IMOOC',__DIR__."\\");
 // define('IMOOC',realpath('/'));
+//核心文件位置
 define('CORE',IMOOC.'core\\');
+//项目文件
 define('APP',IMOOC.'app\\');
 define('MODULE','app');
 //开启调试模式
 define('DEBUG',TRUE);
+//引入composer错误处理
+include "vendor/autoload.php";
+
 if(DEBUG){
+	$whoops = new \Whoops\Run;
+	$errorTitle = '框架出错了';
+	$option = new \Whoops\Handler\PrettyPageHandler();
+	$option->setPageTitle($errorTitle);
+	$whoops->pushHandler($option);
+	$whoops->register();
 	ini_set('display_error','On');
 }else{
 	ini_set('display_error','Off');
 }
+// dump($_SERVER);
+//加载函数库
 include CORE.'common\function.php';
+//加载核心文件
 include CORE.'imooc.php';
 spl_autoload_register('\core\imooc::load');//当new一个类的时候如果不存在会触发这个参数
 \core\imooc::run();
